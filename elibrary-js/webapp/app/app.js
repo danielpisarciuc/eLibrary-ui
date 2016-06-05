066b0b24e6674c988049657d590ac5f6180524b6
Ionut <ionutdny9@gmail.com>
2016-06-05 23:55:41 +0300

    [IP] - clean code

--- elibrary-js/webapp/app/app.js
+++ elibrary-js/webapp/app/app.js
@@ -18,7 +18,7 @@
                 controllerAs: 'vm'
             })
             .otherwise({
-                redirectTo: '/admin'
+                redirectTo: '/authorBooks'
             });
     }
 
